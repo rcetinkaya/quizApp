26519ca03be3ad2eced9c4d1138a327e1073b2c0
Ramazan ÇETİNKAYA <rmzncetinkaya@outlook.com>
2024-08-25 21:35:04 +0300

    updates main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,21 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-purple-500 ">
-        <div className="flex flex-col h-auto p-8 rounded items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white ">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-purple-500 relative ">
+        <div className="w-full h-full absolute z-0">
+        <img src="siha.png" />
+        </div>
+        <div className="absolute top-5 left-5">
+          <img src="baykar.png" />
+        </div>
+        <div className="flex flex-col h-auto p-8 z-10 rounded items-center justify-center bg-gradient-to-b from-[#2f026d3b] to-[#15162c] text-white ">
           <h1 className="text-4xl font-bold mb-8">Hoş Geldiniz!</h1>
           <p className="text-lg mb-8">Bu sınav 10 sorudan oluşmaktadır. Başlamak için aşağıdaki butona tıklayın.</p>
+          <p className="text-lg mb-8">Soru başlıkları kopyalanamaz</p>
+          <p className="text-lg mb-8">Sorunun ilk 10 saniyesi işaretleme yapılamaz</p>
+          <p className="text-lg mb-8">Sayfa yenilemesiyle ekstra süre kazanılamaz</p>
+          <p className="text-lg mb-8">Soruda geçen süre kaydedilmektedir.</p>
+          <p className="text-lg mb-8">Sınav sonucunda doğru ve işaretlenen şıklar görunmektedir.</p>
           <Link href="/quiz">
             <button className="py-2 px-4 bg-purple-500 text-white rounded hover:bg-purple-700">
               Sınavı Başlat
