42f3ec4b528dd02f6e54a3923664e4d273423828
Ramazan ÇETİNKAYA <rmzncetinkaya@outlook.com>
2024-08-30 00:35:53 +0300

    update - quiz page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,6 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-purple-500 relative ">
-        <div className="w-full h-full absolute z-0">
-        <img src="siha.png" />
-        </div>
-        <div className="absolute top-5 left-5">
-          <img src="baykar.png" />
-        </div>
         <div className="flex flex-col h-auto p-8 z-10 rounded items-center justify-center bg-gradient-to-b from-[#2f026d3b] to-[#15162c] text-white ">
           <h1 className="text-4xl font-bold mb-8">Hoş Geldiniz!</h1>
           <p className="text-lg mb-8">Bu sınav 10 sorudan oluşmaktadır. Başlamak için aşağıdaki butona tıklayın.</p>
